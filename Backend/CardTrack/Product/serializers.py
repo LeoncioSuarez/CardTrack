22c9964ac78b73706f3468212e39fa0a0f3a4a62
JuanVargas1804 <juanandresvg1804@gmail.com>
2025-10-03 11:22:09 -0400

    Cambios en nombres, y hasheo de contraseñas

--- Backend/CardTrack/Product/serializers.py
+++ Backend/CardTrack/Product/serializers.py
@@ -1,28 +1,47 @@
 from rest_framework import serializers
+from django.contrib.auth.hashers import make_password
 from .models import User, Board, Column, Card
+
 
 class CardSerializer(serializers.ModelSerializer):
     class Meta:
         model = Card
         fields = '__all__'
 
+
 class ColumnSerializer(serializers.ModelSerializer):
     cards = CardSerializer(many=True, read_only=True)
-    
+
     class Meta:
         model = Column
         fields = '__all__'
 
+
 class BoardSerializer(serializers.ModelSerializer):
     columns = ColumnSerializer(many=True, read_only=True)
-    
+
     class Meta:
         model = Board
         fields = '__all__'
 
+
 class UserSerializer(serializers.ModelSerializer):
     boards = BoardSerializer(many=True, read_only=True)
-    
+    password = serializers.CharField(write_only=True, required=False)
+
     class Meta:
         model = User
-        fields = '__all__'
+        exclude = ('password_hash',)
+
+    def create(self, validated_data):
+        password = validated_data.pop('password', None)
+        if password:
+            validated_data['password_hash'] = make_password(password)
+        return super().create(validated_data)
+
+    def update(self, instance, validated_data):
+        password = validated_data.pop('password', None)
+        if password:
+            instance.password_hash = make_password(password)
+            instance.save()
+        return super().update(instance, validated_data)
